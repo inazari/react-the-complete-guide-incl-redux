b24cb43f93e5e3c9fe56afae46f9a2b6b1e7f352
Nazar Sosnovskyy <nsosnovskyy@liaisonedu.com>
2020-08-14 10:05:51 +0300

    pass params to Burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import Burger from "../../components/Burger/Burger";
 
 
 class BurgerBuilder extends Component{
+   state = {
+       ingredients: {
+           salad: 0,
+           bacon: 0,
+           cheese: 0,
+           meat: 0
+       }
+   }
 
     render() {
         return (<Fragment>
-            <Burger />
+            <Burger ingredients={this.state.ingredients}/>
             <div>Build Control</div>
         </Fragment>)
     }
